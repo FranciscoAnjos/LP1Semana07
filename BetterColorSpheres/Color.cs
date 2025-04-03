ae1c5ed8e470a9c54e8203b4adb0720d644f5f15
FranciscoAnjos <fcariaanjos@gmail.com>
2025-04-03 18:23:11 +0100

    modify color to have properties

---
+++ BetterColorSpheres/Color.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BetterColorSpheres
+{
+    public class Color
+    {
+        public int Red { get; }
+        public int Green { get; }
+        public int Blue { get; }
+        
+        private int _alpha;
+        
+        public int Alpha
+        {
+            get => _alpha;
+            set => _alpha = Clamp(value);
+        }
+
+        public Color(int red, int green, int blue, int alpha = 255)
+        {
+            Red = Clamp(red);
+            Green = Clamp(green);
+            Blue = Clamp(blue);
+            Alpha = alpha;
+        }
+
+        public int Grey => (Red + Green + Blue) / 3;
+
+        private int Clamp(int value)
+        {
+            return Math.Max(0, Math.Min(255, value));
+        }
+
+        public override string ToString()
+        {
+            return $"Color(R: {Red}, G: {Green}, B: {Blue}, A: {Alpha})";
+        }
+    }
+}
---
+++ BetterColorSpheres/Sphere.cs
@@ -0,0 +1,43 @@
+﻿using BetterColorSpheres;
+
+namespace ColorSpheres
+{
+    public class Sphere
+    {
+        private readonly Color _color;
+        private int _radius;
+        private int _timesThrown;
+
+        public Sphere(Color color, int radius)
+        {
+            _color = color;
+            _radius = radius;
+            _timesThrown = 0;
+        }
+
+        public Color GetColor() => _color;
+
+        public void Pop()
+        {
+            _radius = 0;
+        }
+
+        public void Throw()
+        {
+            if (_radius > 0)
+            {
+                _timesThrown++;
+            }
+        }
+
+        public int GetTimesThrown()
+        {
+            return _timesThrown;
+        }
+
+        public override string ToString()
+        {
+            return $"Sphere(Color: {_color}, Radius: {_radius}, Times Thrown: {_timesThrown})";
+        }
+    }
+}
