f17e6bea2d39ec2d6e3f19a8551dfd439868c0f1
FranciscoAnjos <fcariaanjos@gmail.com>
2025-04-03 18:11:37 +0100

    create Sphere.cs

---
+++ ColorSpheres/Sphere.cs
@@ -0,0 +1,39 @@
+﻿namespace ColorSpheres
+{
+    public class Sphere
+    {
+        private readonly Color _color;
+        private int _radius;
+        private int _timesThrown;
+
+        public Sphere(Color color, int radius)
+        {
+            _color = color;
+            _radius = radius;
+            _timesThrown = 0;
+        }
+
+        public void Pop()
+        {
+            _radius = 0;
+        }
+
+        public void Throw()
+        {
+            if (_radius > 0)
+            {
+                _timesThrown++;
+            }
+        }
+
+        public int GetTimesThrown()
+        {
+            return _timesThrown;
+        }
+
+        public override string ToString()
+        {
+            return $"Sphere(Color: {_color}, Radius: {_radius}, Times Thrown: {_timesThrown})";
+        }
+    }
+}
