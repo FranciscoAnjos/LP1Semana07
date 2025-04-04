0d30bfb42ec1faf46f3fa40485d7e919b5cff08f
FranciscoAnjos <fcariaanjos@gmail.com>
2025-04-04 14:14:59 +0100

    add XP and Health

--- MyRPG/Player.cs
+++ MyRPG/Player.cs
@@ -1,61 +1,11 @@
-﻿namespace PlayerStats
+﻿namespace MyRPG
 {
     public class Player
     {
-        private float _highScore;
-        private int _playedGames;
-        private int _wonGames;
+        private int xp;
+        private float health;
 
-        public float HighScore
-        {
-            get => _highScore;
-            set
-            {
-                if (value > _highScore)
-                {
-                    _highScore = value;
-                }
-            }
-        }
+        public string Name get
 
-        public string Name { get; }
-
-        public float WinRate
-        {
-            get
-            {
-                if (_playedGames == 0)
-                {
-                    return 0;
-                }
-                return (float)_wonGames / _playedGames;
-            }
-        }
-
-        public Player(string name)
-        {
-            Name = name;
-            _highScore = 0f;
-            _playedGames = 0;
-            _wonGames = 0;
-        }
-
-        public void PlayGame(bool win)
-        {
-            _playedGames++;  
-
-            if (win)
-            {
-                _wonGames++;  
-            }
-        }
-
-        public override string ToString()
-        {
-            return $"Player: {Name}, HighScore: {_highScore}, " +
-                   $"PlayedGames: {_playedGames}, " +
-                   $"WonGames: {_wonGames}, " +
-                   $"WinRate: {WinRate:P2}";
-        }
     }
 }
